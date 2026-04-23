555697a442c6def9b3f49c45fed19f071e831cd8
azlancodeshere <azlan.codes@gmail.com>
2026-04-23 16:51:52 +0530

    fix product image url

--- shoppit_app/src/components/home/HomeCard.jsx
+++ shoppit_app/src/components/home/HomeCard.jsx
@@ -15,7 +15,7 @@ const HomeCard = ( {product} ) => {
           
           <div className={styles.cardImgWrapper}>
             <img
-              src={`${BASE_URL}${product.image}`}
+              src={`{product.image}`}
               className={styles.cardImgTop}
               alt="Product Image"
             />
