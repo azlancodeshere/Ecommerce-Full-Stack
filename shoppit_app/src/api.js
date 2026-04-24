b0f833ae54de9e5f3d705555561feaa8004e74af
azlancodeshere <azlan.codes@gmail.com>
2026-04-24 15:54:37 +0530

    use env for api url

--- shoppit_app/src/api.js
+++ shoppit_app/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import {jwtDecode} from "jwt-decode"
 
-export const BASE_URL = "https://ecommerce-full-stack-g7az.onrender.com/"
+export const BASE_URL = import.meta.env.VITE_API_URL;
 
 const api = axios.create({
     baseURL: BASE_URL,
