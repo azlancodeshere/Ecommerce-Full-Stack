3f9edf4fbe87f43c8a685f384583346c0761d7c9
azlancodeshere <azlan.codes@gmail.com>
2026-04-24 21:17:42 +0530

    fix: replace broken dummyimage URL with CSS skeleton placeholder

--- shoppit_app/src/components/product/ProductPagePlaceHolder.jsx
+++ shoppit_app/src/components/product/ProductPagePlaceHolder.jsx
@@ -6,32 +6,31 @@ const ProductPagePlaceHolder = () => {
       <div className="container px-4 px-lg-5 my-5">
         <div className="row gx-4 gx-lg-5 align-items-center">
 
-          {/* Image Placeholder */}
+          
           <div className="col-md-6">
-            <img
-              className="card-img-top mb-5 mb-md-0"
-              src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg"
-              alt="..."
-            />
+            <div
+              className="placeholder-glow w-100 mb-5 mb-md-0"
+            >
+              <span
+                className="placeholder w-100 d-block rounded"
+                style={{height: "400px"}}
+              ></span>
+            </div>
           </div>
 
           {/* Text Placeholder */}
           <div className="col-md-6">
             <div className="placeholder-glow">
-
               <span className="placeholder col-4"></span>
               <span className="placeholder col-12"></span>
               <span className="placeholder col-4"></span>
-
-
               <p>
-              <span className="placeholder col-6"></span>
-              <span className="placeholder col-8"></span>
-              <span className="placeholder col-12"></span>
-              <span className="placeholder col-12"></span>
-              <span className="placeholder col-12"></span>
+                <span className="placeholder col-6"></span>
+                <span className="placeholder col-8"></span>
+                <span className="placeholder col-12"></span>
+                <span className="placeholder col-12"></span>
+                <span className="placeholder col-12"></span>
               </p>
-
             </div>
           </div>
 
